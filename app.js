de5954ddf6c649a95a06e004bc77acbf6c6abc9c
mwonuse <crystaltrucker7@gmal.com>
2020-10-19 22:44:50 +0530

    initial version

---
+++ app.js
@@ -0,0 +1,39 @@
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  if (socket.handshake.query.username) {
+    const username = socket.handshake.query.username;
+    console.log(username, "created a room and joined");
+    console.log(username, "connected");
+    socket.join(username);
+  }
+
+  if (socket.handshake.query.target) {
+    const target = socket.handshake.query.target;
+    socket.join(target);
+    console.log("a user joined the room", target);
+  }
+
+  socket.on("chat message", (body) => {
+    if(body.origin){
+      io.to(body.origin).emit("chat message", body.msg);
+    }else if(body.target){
+      io.to(body.target).emit("chat message", body.msg);
+
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+http.listen(3000, () => {
+  console.log("listening on *:3000");
+});
