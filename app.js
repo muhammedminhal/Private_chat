04b26efff823e31ef6dc2f144c76fb5d3a602df3
minhal <minhalkizhakkayil98@gmail.com>
2020-11-18 12:05:27 +0530

    private chatting

--- app.js
+++ app.js
@@ -34,6 +34,6 @@ io.on("connection", (socket) => {
   });
 });
 
-http.listen(3000, () => {
-  console.log("listening on *:3000");
+http.listen(8000, () => {
+  console.log("listening on *:8000");
 });
